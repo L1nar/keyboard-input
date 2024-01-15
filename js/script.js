11b04414d2e823ba50b3d3cdbd16500b6f9b8180
L1nar <122251664+L1nar@users.noreply.github.com>
2024-01-15 16:39:00 +0500

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,16 +1,25 @@
 const phone = document.getElementById('phone');
 
 phone.addEventListener('keydown', function (event) {
+    console.log(event.key);
     let isDigit = false;
-    if (event.key >= 0 && event.key <= 0) {
+    let isDash = false;
+    let isControl = false;
+    if (event.key >= 0 && event.key <= 9) { // Проверка ввода цифр
         isDigit = true;
     }
-    if (isDigit == true) {
-        console.log('Число')
-    } else {
-        console.log('Не число');
+    if (event.key == '-') { // Проверка ввода '-'
+        isDash = true;
     }
-    /*console.log(event.key); event - событие. При возникновнии событий, бразуер автоматически передает в функцию-обработчик объект,
- содержащий подробную информацию об этом событии. При описании функции этот параметр обычно называют 'event' (можно так же встреить evt или просто e).
- Свойство 'key' объекта 'event' содержит нажатый на клавиатуре символ.*/
+    if (event.key == 'arrowleft' || 'arrowright' || 'BackSpace') { // Проверка нажатия на клавиши управления курсором или backspace
+        isControl = true;
+    }
+    if (!isDigit && !isDash && !isControl) { // Если нажата не цифра, не тире, и не клавиши управления курсором или backspace, то отменить ввод
+        event.preventDefault();
+    }
 });
+
+/*console.log(event.key); event - событие. При возникновнии событий, бразуер автоматически передает в функцию-обработчик объект,
+содержащий подробную информацию об этом событии. При описании функции этот параметр обычно называют 'event' (можно так же встреить evt или просто e).
+Свойство 'key' объекта 'event' содержит нажатый на клавиатуре символ.*/
+// preventDefault - отмена действия по умолчанию
